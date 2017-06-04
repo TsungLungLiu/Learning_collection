78e11472c165c30bfcaf17d618750e8e5e8f948b
TsungLungLiu <j5877758777@gmail.com>
2017-06-04 10:50:26 -0400

    Add project files.

---
+++ Learning_collection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learning_collection.Startup))]
+namespace Learning_collection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
